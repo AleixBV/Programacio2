53bba921c5fbe096e730588e7776f9809876ce6f
Aleix <aleixborrellvives@gmail.com>
2015-03-02 12:01:01 +0100

    point2f unit test

--- UnitTest1/unittest1.cpp
+++ UnitTest1/unittest1.cpp
@@ -1,6 +1,6 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
-#include "../Programacio2/Point2f.h"
+#include "../Programacio2/Point2f.cpp"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -9,14 +9,38 @@ namespace UnitTest1
 	TEST_CLASS(UnitTest1)
 	{
 	public:
-		
-		TEST_METHOD(TestMethod1)
-		{
-			Point2f punt;
 
-			punt.setZero();
-			Assert::IsTrue(punt.checkZero());
+		/*TEST_METHOD(TestMethod_operator)
+		{
+		Point2f point1, point2;
+		punt1.x = 5;
+		punt1.y = 11;
+
+		punt2.x = 7;
+		punt2.y = 8;
+
+		punt1.operator+(punt2);
+		Assert::AreEqual(punt1.x + punt2.y);
+
+		}*/
+		
+		TEST_METHOD(TestMethod_isZero)
+		{
+			Point2f point;
+
+			point.x = 0.0f;
+			point.y = 0.0f;
+			Assert::IsTrue(point.isZero());
 		}
+
+		TEST_METHOD(TestMethod_setZero)
+		{
+			Point2f point;
+
+			point.setZero();
+			Assert::IsTrue(point.isZero());
+		}
+
 
 	};
 }

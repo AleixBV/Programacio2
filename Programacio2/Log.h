3a60f3498ceb6a7650398ab5c9fbf91d90c592a5
Aleix <aleixborrellvives@gmail.com>
2015-03-13 12:50:30 +0100

    Log arreglat

--- Programacio2/Log.h
+++ Programacio2/Log.h
@@ -1,25 +1,5 @@
 #include <Windows.h>
-#include <assert.h>
-#include <string.h>
 #include <stdio.h>
 
-
-void Log(const char* cadena, char* imatge, float valor)
-{
-	char output[500];
-	assert(cadena);
-	assert(imatge);
-	assert(valor);
-	sprintf_s(output, 500, cadena, imatge, valor);
-	OutputDebugString(output);
-}
-
-int main(int argc, char** argv)
-{
-	char textura[100] = "my textura.png";
-
-
-	Log("Cargando Textura: %s en %f segundos\n", textura, 3.1416f);
-
-	getchar();
-}
+#define LOG(format, ...) log(__FILE__, __LINE__, format, __VA_ARGS__);
+void log(const char file[], int line, const char* format, ...);
